1b7535a20d584f62b16a76197a9a51364588b5b5
Mark Broeders <mail@markbroeders.nl>
2025-08-28 08:25:10 +0200

    Add some comments for new configuration

--- hosts/asus/default.nix
+++ hosts/asus/default.nix
@@ -16,10 +16,11 @@
 		[ # Include the results of the hardware scan.
 		./hardware-configuration.nix
 
+		# I DONT KNOW IF I STILL WANT ALL THESE MODULES
+		# IT IS JUST ONE SYSTEM
 		../../modules/system.nix
 		../../modules/bluetooth.nix
 		../../modules/sound.nix
-		../../modules/X11.nix
 		../../modules/fonts.nix
 		../../modules/office.nix
 		];
@@ -31,19 +32,20 @@
 	networking.hostName = "wheeler"; # Define your hostname.
 	networking.networkmanager.enable = true;
 
+	# IS THIS STILL NECESSARY WITH GNOME?
 	# Touchpad configuration
-	environment.systemPackages = [
-		pkgs.libinput-gestures
-		pkgs.xdotool
-		pkgs.wmctrl
-	];
+	# environment.systemPackages = [
+	# 	pkgs.libinput-gestures
+	# 	pkgs.xdotool
+	# 	pkgs.wmctrl
+	# ];
 
-	services.xserver.libinput.touchpad = {
-		naturalScrolling = true;
-	};
+	# services.xserver.libinput.touchpad = {
+	# 	naturalScrolling = true;
+	# };
 
-
-	services.tlp.enable = true;
+# DO I NEED THIS WITH GNOME INSTALLED?
+#	services.tlp.enable = true;
 
 # This value determines the NixOS release from which the default
 # settings for stateful data, like file locations and database versions
